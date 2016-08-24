9fe516a7815dde0277361fc9e6d73c9b5c4b9b3a
shway250 <jackson_duhon@yahoo.com>
2016-08-24 12:26:20 -0700

    about to start wednesday coding. finished async API calls.

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,21 +1,53 @@
 var express = require("express");
 var router = express.Router();
 var request = require('request');
+var async = require('async');
 var db = require('../models');
 
-router.get("/search", function(req, res) {
- res.render('pages/search');
-});
 
-router.post('/search', function(req, res){
-  //this is where search words will be searched for useing the apis and user
-  //will be redirected to the results page
-  console.log(req.body);
-});
 
-router.get("/results", function(req, res){
-  //this is where I'll render the results page
-});
+//Search and results Routes
+router.get("/results", function(req, res) {
+  var searchTerm = req.query.search;
+
+  ///////API REQUEST FUNCTIONS!!!
+  function guardian(callback){
+    request({
+      url: 'http://content.guardianapis.com/search',
+      qs: {
+      'api-key': "a8d459cf-2a88-4dc8-b62c-a4913e0b7bb7",
+      'q': searchTerm
+      }
+    },function(error, response, body){
+      if(!error && response.statusCode == 200){
+        var guardianData = JSON.parse(body);
+        callback(null, guardianData.response.results[0]);
+      }
+    });
+  }
+  function nyTimes(callback){
+    request({
+      url: 'https://api.nytimes.com/svc/search/v2/articlesearch.json',
+      qs: {
+      'api-key': "a67bf00f73074416973aeece50e62613",
+      'q': searchTerm
+      }
+    },function(error, response, body){
+      if(!error && response.statusCode == 200){
+        var nyTimesData = JSON.parse(body);
+        callback(null, nyTimesData.response.docs[0]);
+      }
+    });
+  }
+  
+  ////////////ASYNC
+  async.series([guardian, nyTimes], function(err, results) {
+    console.log(results[0]);
+    res.render('pages/results', {guardian: results[0], nyTimes: results[1]});
+  });
+}); 
+
+
 
 router.get("/election", function(req, res){
   ///Election 2016 page
